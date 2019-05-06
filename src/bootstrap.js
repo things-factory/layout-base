f9eebb10f86967c053d6ed63c4651bd32d1afecf
horwengliang <horwengliang@gmail.com>
2019-05-06 18:04:48 +0800

    add dispatch Show Snackbar

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,10 +2,14 @@ import { store } from '@things-factory/shell'
 
 import layout from './reducers/layout'
 import snackbar from './reducers/snackbar'
+import { showSnackbar } from './actions/snackbar'
 
 export default function bootstrap() {
   store.addReducers({
     layout,
     snackbar
+  })
+  document.addEventListener('notify', function(e) {
+    store.dispatch(showSnackbar(e.detail))
   })
 }
