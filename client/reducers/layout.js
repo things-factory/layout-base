38eac4d2b445ade5f5761c462a48cba6d0230cd4
Hearty Oh <heartyoh@hatiolab.com>
2019-06-27 02:23:15 +0900

    v0.1.7

--- client/reducers/layout.js
+++ client/reducers/layout.js
@@ -1,10 +1,16 @@
 import {
   APPEND_HEADERBAR,
+  REMOVE_HEADERBAR,
   APPEND_NAVBAR,
+  REMOVE_NAVBAR,
   APPEND_ASIDEBAR,
+  REMOVE_ASIDEBAR,
   APPEND_FOOTERBAR,
+  REMOVE_FOOTERBAR,
   APPEND_APP_TOOL,
+  REMOVE_APP_TOOL,
   APPEND_CONTEXT_TOOL,
+  REMOVE_CONTEXT_TOOL,
   UPDATE_WIDTH,
   TOGGLE_OVERLAY
 } from '../actions/layout'
@@ -31,10 +37,22 @@ const layout = (state = INITIAL_STATE, action) => {
         headerbars: [...state.headerbars, action.headerbar]
       }
 
+    case REMOVE_HEADERBAR:
+      return {
+        ...state,
+        headerbars: state.headerbars.filter(i => i !== action.headerbar)
+      }
+
     case APPEND_NAVBAR:
       return {
         ...state,
         navbars: [...state.navbars, action.navbar]
+      }
+
+    case REMOVE_NAVBAR:
+      return {
+        ...state,
+        navbars: state.navbars.filter(i => i !== action.navbar)
       }
 
     case APPEND_ASIDEBAR:
@@ -43,10 +61,22 @@ const layout = (state = INITIAL_STATE, action) => {
         asidebars: [...state.asidebars, action.asidebar]
       }
 
+    case REMOVE_ASIDEBAR:
+      return {
+        ...state,
+        asidebars: state.asidebars.filter(i => i !== action.asidebar)
+      }
+
     case APPEND_FOOTERBAR:
       return {
         ...state,
         footerbars: [...state.footerbars, action.footerbar]
+      }
+
+    case REMOVE_FOOTERBAR:
+      return {
+        ...state,
+        footerbars: state.footerbars.filter(i => i !== action.footerbar)
       }
 
     case APPEND_APP_TOOL:
@@ -55,10 +85,22 @@ const layout = (state = INITIAL_STATE, action) => {
         appTools: [...state.appTools, action.tool]
       }
 
+    case REMOVE_APP_TOOL:
+      return {
+        ...state,
+        appTools: state.appTools.filter(i => i !== action.tool)
+      }
+
     case APPEND_CONTEXT_TOOL:
       return {
         ...state,
         contextTools: [...state.contextTools, action.tool]
+      }
+
+    case REMOVE_CONTEXT_TOOL:
+      return {
+        ...state,
+        contextTools: state.appTools.filter(i => i !== action.tool)
       }
 
     case UPDATE_WIDTH:
